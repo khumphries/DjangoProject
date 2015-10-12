f247caf310a2ad29a1bb7e16c7eecf97e76b2c55
Lina He <lh3su@virginia.edu>
2015-10-12 14:30:13 -0400

    lab1 files

---
+++ lab1_fact.py
@@ -0,0 +1,38 @@
+def main():
+    
+    #x = int(input("Enter a number: "))
+    #print(factorial1(x))
+    #print(factorial2(x))
+    test_fact1()
+
+
+def factorial1(n):
+    if(n < 0):
+        raise ValueError('Value cannot be negative')
+    if n == 0:
+        return 1;
+    if n == 1:
+        return 1
+    return factorial1(n-1) + factorial1(n-2);
+
+def factorial2(n):
+    if(n < 0):
+        raise ValueError('Value cannot be negative')
+    if n == 0:
+        return [1]
+    if n == 1:
+        return [1,1]
+
+    factList = [1,1]
+    for i in range (2,n+1):
+        factList.append(factList[i-1]+factList[i-2])
+    return factList;
+        
+def test_fact1():
+    assert factorial1(0)==1, "factorial1 base case 0 false"
+    assert factorial1(1)==1, "factorial1 base case 1 false"
+    assert factorial1(5)==8, "factorial1 n = 5 does not return 8"
+    print ("no errors")
+
+if __name__ == "__main__":
+    main()
