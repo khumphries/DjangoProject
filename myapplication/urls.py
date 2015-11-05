66714fe9cb21f6c696c4b2e0a365ec9cdae2110d
khumphries <khumphries@gannett.com>
2015-11-04 23:00:16 -0500

    Added home and logged_out pages, added a logout functionallity, fixed sign_up to throw error instead of crash when a user enters a taken username, sign_up now automatically logs you in, added links to various pages

--- myapplication/urls.py
+++ myapplication/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('myapplication.views',
     url(r'^sign_up/$',views.sign_up, name='sign_up'),
     url(r'^sign_up_complete/$',views.sign_up_complete, name='sign_up_complete'),
     url(r'^login/$', views.login_user, name='login'),
+    url(r'^home/$', views.home_page, name='home'),
+    url(r'^logout/$', views.logout_user, name='logout'),
 )
--- myapplication/views.py
+++ myapplication/views.py
@@ -7,52 +7,65 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib import auth
 
 from myapplication.models import Document
 from myapplication.forms import DocumentForm
 from myapplication.forms import UserForm
 
+
 def list(request):
-	# Handle file upload
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            newdoc = Document(docfile = request.FILES['docfile'])
-            newdoc.save()
+    if request.user.is_authenticated():
+	   # Handle file upload
+        if request.method == 'POST':
+            form = DocumentForm(request.POST, request.FILES)
+            if form.is_valid():
+                newdoc = Document(docfile = request.FILES['docfile'])
+                newdoc.save()
 
-            # Redirect to the document list after POST
-            return HttpResponseRedirect(reverse('myapplication.views.list'))
+                # Redirect to the document list after POST
+                return HttpResponseRedirect(reverse('myapplication.views.list'))
+        else:
+            form = DocumentForm() # A empty, unbound form
+
+        # Load documents for the list page
+        documents = Document.objects.all()
+
+        # Render list page with the documents and the form
+        return render_to_response(
+            'myapplication/list.html',
+            {'documents': documents, 'form': form},
+            context_instance=RequestContext(request)
+        )
     else:
-        form = DocumentForm() # A empty, unbound form
-
-    # Load documents for the list page
-    documents = Document.objects.all()
-
-    # Render list page with the documents and the form
-    return render_to_response(
-        'myapplication/list.html',
-        {'documents': documents, 'form': form},
-        context_instance=RequestContext(request)
-    )
-
+        return render(request, 'myapplication/auth.html')
 #For signup page
 def sign_up(request):
     #if we need to process form data
+    state = 'Please sign up below'
     if request.method == 'POST':
         form = UserForm(request.POST)
         #checks if data is valid and then 
         if form.is_valid():
-            user = User.objects.create_user(form.cleaned_data['username'],'',form.cleaned_data['password'])
-            user.save()
-            return HttpResponseRedirect(reverse('myapplication.views.sign_up_complete'))
+            if User.objects.filter(username=form.cleaned_data['username']).exists():
+                state = "That user name is already taken, please try another."
+            else:
+                user = User.objects.create_user(form.cleaned_data['username'],'',form.cleaned_data['password'])
+                user.save()
+                user_authentication = authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password'])
+                login(request, user_authentication)
+                return HttpResponseRedirect(reverse('myapplication.views.sign_up_complete'))
 
     else:
         form = UserForm()
 
-    return render(request,'myapplication/sign_up.html', {'form' : form})
+    return render(request,'myapplication/sign_up.html', {'form' : form, 'state' : state})
 #For page after sign up
 def sign_up_complete(request):
-    return render(request, 'myapplication/sign_up_complete.html')
+    if request.user.is_authenticated():
+        return render(request, 'myapplication/sign_up_complete.html')
+    else:
+        return render(request, 'myapplication/auth.html')
 
 def login_user(request):
     state = "Please log in below..."
@@ -67,12 +80,23 @@ def login_user(request):
             if user.is_active:
                 login(request, user)
                 state = "You're successfully logged in!"
-                return HttpResponseRedirect(reverse('myapplication.views.list'))
+                return HttpResponseRedirect(reverse('myapplication.views.home_page'))
             else:
                 state = "Your account is not active, please contact the site admin."
         else:
             state = "Your username and/or password were incorrect."
 
     return render_to_response('myapplication/auth.html', RequestContext(request, {'state':state, 'username': username}))
-    
+
+def home_page(request):
+    if request.user.is_authenticated():
+        return render(request, 'myapplication/home.html')
+    else:
+        return render(request, 'myapplication/auth.html')
+def logout_user(request):
+    if request.user.is_authenticated():
+        auth.logout(request)
+        return render(request, 'myapplication/loggedout.html')
+    else:
+        return render(request, 'myapplication/auth.html')
 # Create your views here.
