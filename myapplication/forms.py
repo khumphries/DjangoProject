90402cf348363c465371c37ed270b0e7a09e5468
khumphries <khumphries@gannett.com>
2015-11-21 21:28:41 -0500

    Added the sitemanager roll and functionallity to add groups and op users

--- myapplication/forms.py
+++ myapplication/forms.py
@@ -19,3 +19,14 @@ class MessageForm(forms.Form):
 	msg = forms.CharField(label="Message", max_length=500)
 	receiver = forms.CharField(label="Recipient", max_length=30)
 
+class SiteManagerUserForm(forms.Form):
+    username = forms.CharField(label='User Name', max_length=30)
+    CHOICES = (('1','Make User Site Manager',),('2','Suspend User',),('3','Reactivate User',))
+    choice_field = forms.ChoiceField(widget=forms.RadioSelect,choices=CHOICES)
+
+class SiteManagerGroupForm(forms.Form):
+    username = forms.CharField(label='User Name', max_length=30, required=False)
+    groupname = forms.CharField(label='Group Name', max_length=80)
+    CHOICES = (('1','Create group',),('2','Add user to group',),('3','Remove user from group',))
+    choice_field = forms.ChoiceField(widget=forms.RadioSelect,choices=CHOICES)
+
