4e21103655ee05d6ed539d404fe87922affee681
linahe <lh3su@virginia.edu>
2015-12-06 22:03:27 -0500

    merging database

---
+++ myapplication/migrations/0023_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('myapplication', '0022_document_encrypt'),
+        ('myapplication', '0022_document_content'),
+    ]
+
+    operations = [
+    ]
